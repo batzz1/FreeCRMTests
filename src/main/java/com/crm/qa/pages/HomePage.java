2921cef0979a2b70d236b3010ad3e367a1579ed8
Gaurav Sharma <grvsharma@hotmail.com>
2018-12-23 22:26:40 +0530

    Added OR on HomePage

--- src/main/java/com/crm/qa/pages/HomePage.java
+++ src/main/java/com/crm/qa/pages/HomePage.java
@@ -1,7 +1,26 @@
 package com.crm.qa.pages;
 
+import com.crm.qa.base.TestBase;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class HomePage {
+public class HomePage extends TestBase {
 
+    @FindBy(xpath = "//td[contains(text(),'Gaurav')]")
+    private WebElement usernameLabel;
+
+    @FindBy(xpath = "//a[contains(text(),'Contacts')]")
+    private WebElement contactsLink;
+
+    @FindBy(xpath = "//a[contains(text(),'Deals')]")
+    private WebElement dealsLink;
+
+    @FindBy(xpath = "//a[contains(text(),'Tasks)]")
+    private WebElement tasksLink;
+
+    public HomePage() {
+        PageFactory.initElements(driver, this);
+    }
 }
