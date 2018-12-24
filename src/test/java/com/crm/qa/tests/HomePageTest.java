dd32edba289cffde3819e474d19b1a861031b99c
Gaurav Sharma <grvsharma@hotmail.com>
2018-12-24 22:31:02 +0530

    Added HomePage Title Test

---
+++ src/test/java/com/crm/qa/tests/HomePageTest.java
@@ -0,0 +1,37 @@
+package com.crm.qa.tests;
+
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends TestBase {
+
+    LoginPage loginPage;
+    HomePage homePage;
+
+    public HomePageTest() {
+        super();
+    }
+
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+        initialization();
+        loginPage = new LoginPage();
+        homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+    }
+
+    @Test
+    public void verifyHomePageTitleTest() {
+        String title = homePage.verifyHomePageTitle();
+        Assert.assertEquals(title, "CRMPRO" , "Home Page Title does not match");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
