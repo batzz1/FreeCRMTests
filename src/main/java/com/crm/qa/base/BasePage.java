82f3ee16b27edea7d0692b1ab103cb3cf76e9ba8
Gaurav Sharma <grvsharma@hotmail.com>
2019-01-28 15:31:40 +0530

    Added Operation methods to BaseClass

--- src/main/java/com/crm/qa/base/BasePage.java
+++ src/main/java/com/crm/qa/base/BasePage.java
@@ -1,7 +1,12 @@
 package com.crm.qa.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.Set;
 
 public class BasePage {
 
@@ -11,5 +16,56 @@ public class BasePage {
     public BasePage(WebDriver driver) {
         this.driver = driver;
         wait = new WebDriverWait(this.driver, 40);
+    }
+
+    public WebElement waitForElementToBeVisible(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForElementToBeVisible(WebElement element, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public WebElement waitForElementToBeClickable(WebElement element, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public WebElement waitForElementToBeClickable(By by, int timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        return wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    public void waitForElementToBeClickable(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitForElementToBeClickable(By by) {
+        wait.until(ExpectedConditions.elementToBeClickable(by));
+    }
+
+    public void waitForPresenceOfAllElements(By by) {
+        sleep(3000);
+        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
+    }
+
+    public void sleep(final long millis) {
+        System.out.println((String.format("sleeping %d ms", millis)));
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void switchToNewTab() {
+        Set<String> allWindows = driver.getWindowHandles();
+        for (String child : allWindows) {
+            if (!driver.getWindowHandle().equalsIgnoreCase(child))
+                driver.switchTo().window(child);
+            sleep(3000);
+
+        }
     }
 }
